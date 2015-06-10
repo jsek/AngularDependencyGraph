b0b77ed6043ef23aafa8d0c5c51eb20412c5e6a2
jsek <jsek.0.0.0.2@gmail.com>
2015-06-10 02:41:59 +0200

    Migrated navigation utilities

---
+++ gui/app/navigation/MainViewService.ts
@@ -0,0 +1,101 @@
+﻿/// <reference path="../../../typings/angularjs/angular.d.ts"/>
+/// <reference path="../../../typings/jquery/jquery.d.ts"/>
+
+var jade = require('jade');
+
+class MainViewService {
+
+    private _history = [];
+    private _templates = [];
+    private _wasAlreadySet = false;
+    private _shadowDOM = $('<div>');
+    
+    private _compilator = () => {};
+
+    private _listeners = {};
+
+    private viewsRootDirectory = 'gui/views/';
+    
+    public container: JQuery;
+    public sidebar: JQuery;
+    public defaultScope: ng.IRootScopeService;
+    
+    protected currentTemplate;
+    
+    constructor($compile, $rootScope) {
+        this._compilator = $compile;
+        this.container = $('.main');
+        this.sidebar = $('.sidebar');
+        this._shadowDOM.insertBefore(this.container).hide();
+        this.defaultScope = $rootScope;   
+    }
+                
+    set(filename, $scope?, ignoreHistory? = false) {
+
+        if (!ignoreHistory) {
+            this._history.push(filename);
+        }
+
+        this.currentTemplate = filename;
+
+        this.trigger('change', filename)
+
+        if (this._templates[filename]?) {
+            this.container.children().detach().appendTo(this._shadowDOM);
+            this._shadowDOM.children("[data-template='#{filename}']").detach().appendTo(this.container);
+            this._wasAlreadySet = true;
+        } else {
+            var template = jade.compileFile(this.viewsRootDirectory + filename);
+            var compiledHTML = this._compilator(template())($scope || this.defaultScope)
+            
+            var newContent = $("<div data-template='#{filename}'></div>");
+            newContent.html(compiledHTML);
+
+            this.container.children().detach().appendTo(this._shadowDOM);
+            this.container.append(newContent);
+
+            this._templates[filename] = newContent
+            this._wasAlreadySet = false;
+        }
+    }
+
+    find(selector) {
+        this.container.find(selector);
+    }
+
+    wasAlreadySet() { 
+        return this._wasAlreadySet; 
+    }
+        
+    back() {
+        this._history.pop();
+        this.set(this._history.last(), null, true);
+    }
+
+    expand() {
+        this.sidebar.addClass('slide-out');
+        this.container.addClass('expanded');
+    }
+    
+    collapse() {
+        this.sidebar.removeClass('slide-out');
+        this.container.removeClass('expanded');
+    }
+
+    trigger(event, data) {
+        for (var listener of this._listeners[event]) {
+            listener(data)
+        } 
+    }
+
+    on(events, fn) {
+        for (event of events.split(',')) {
+            this._listeners[event] = this._listeners[event] || []
+            this._listeners[event].push(fn);
+        }
+    } 
+
+}
+
+angular.module('app')
+.service('mainViewService', MainViewService);
---
+++ gui/app/navigation/SidebarController.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="../../../typings/angularjs/angular.d.ts"/>
+
+class SidebarController {
+    
+    constructor(
+        $scope: any, 
+        mainViewService: MainViewService, 
+        currentProjectService, 
+        projectRepositoryService) {
+        
+        $scope.projects = projectRepositoryService.getProjects();
+
+        mainViewService.on('change', (filename) => {
+            $scope.currentView = filename;
+        });
+        
+        $scope.goHome = () => {
+            mainViewService.set('pages/intro.jade');
+        };
+        
+        projectRepositoryService.on('add', (project) => {
+            $scope.projects.push(project);
+            $scope.$apply();
+        });
+        
+        $scope.open = (projectId) => {
+            var project = $scope.projects.find((x) => x.id === projectId);
+            mainViewService.set('pages/project.jade');
+            currentProjectService.set(project);
+        };
+            
+        console.log('>> [Sidebar] loaded');
+    }
+}
+
+angular.module('app')
+.service('sidebarController', SidebarController);
